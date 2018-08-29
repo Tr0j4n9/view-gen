6298f1ca849634d5a6b7bcd11045b2a2a3123981
Enzo Gladiadis <30397502+Tr0j4n9@users.noreply.github.com>
2018-08-30 10:27:50 +1200

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function sleep(ms){
     })
 }
 
-start()
+async function startBrowser (){
+    const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox', '--proxy-server=138.201.101.33:443']});
+    const mail = await browser.newPage()
+    const flamepage = await browser.newPage()
+    const page = await browser.newPage();
+    const client = page._client;
+    await flamepage.goto('https://flamecoin.co');
+    await sleep(10000)
+    async function stop() {
+        browser.close()
+}
+
+
+
+startBrowser()
